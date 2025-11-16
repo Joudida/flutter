851855a4f2ce9c8df003847097742dfbe9e7c406
joudi dandachi <52230762@students.liu.edu.lb>
2025-11-16 20:14:17 +0200

    Initial commit - daily meal tracker

---
+++ lib/meal.dart
@@ -0,0 +1,8 @@
+class Meal {
+  final String name;
+  final int calories;
+  bool eaten;
+
+  Meal({required this.name,required this.calories, this.eaten =false});
+  
+}
---
+++ lib/meals_list.dart
@@ -0,0 +1,8 @@
+import 'meal.dart';
+
+final List<Meal> meals = [
+  Meal(name: ' Breakfast - Omelette', calories: 250),
+  Meal(name:'Lunch - Grilled Chicken' , calories: 400),
+  Meal(name: 'Dinner - Salad', calories: 200),
+  Meal(name:'Snack - Fruit', calories: 100),
+];
