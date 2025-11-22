ecad4bf99cdb56ebcb745f75d1c8cad18fa53f5c
joudi dandachi <52230762@students.liu.edu.lb>
2025-11-22 11:15:45 +0200

    add product image to assets

--- lib/main.dart
+++ lib/main.dart
@@ -564,6 +564,7 @@ class _ProductCardState extends State<ProductCard> {
                //slide
                //stylee
                //navigationn
+               //pubsec
             ),
           ),
         ],
