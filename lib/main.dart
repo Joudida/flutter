eca8d20d11c0c73b15aa86f0b03c94e8f2aa2a12
joudi dandachi <52230762@students.liu.edu.lb>
2025-11-22 11:00:33 +0200

    add top bar with three categories

--- lib/main.dart
+++ lib/main.dart
@@ -554,6 +554,7 @@ class _ProductCardState extends State<ProductCard> {
 
                   //test
                   //scaffold
+                  //categories
             ),
           ),
         ],
