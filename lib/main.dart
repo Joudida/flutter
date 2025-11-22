f5a4c34d6cefb79759d30a1fded84edf815c612d
joudi dandachi <52230762@students.liu.edu.lb>
2025-11-22 11:01:24 +0200

    add sort by and filter row ui

--- lib/main.dart
+++ lib/main.dart
@@ -555,6 +555,7 @@ class _ProductCardState extends State<ProductCard> {
                   //test
                   //scaffold
                   //categories
+                  //ui
             ),
           ),
         ],
